f4485890dcc9dd341a9ff563044006fdbc58644c
EDWARD <edsztukowski@gmail.com>
2018-04-18 17:30:06 -0600

    adding all new files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,63 +1,84 @@
 'use strict';
 
-var app = {
-  title: 'Indecision App',
-  subtitle: 'Let computers think for you',
-  options: ['One', 'Two']
+var _index = require('/Users/edwardsztukowski/node_modules/redbox-react/lib/index.js');
+
+var _index2 = _interopRequireDefault(_index);
+
+var _index3 = require('/Users/edwardsztukowski/node_modules/react-transform-catch-errors/lib/index.js');
+
+var _index4 = _interopRequireDefault(_index3);
+
+var _react2 = require('react');
+
+var _react3 = _interopRequireDefault(_react2);
+
+var _index5 = require('/Users/edwardsztukowski/node_modules/react-transform-hmr/lib/index.js');
+
+var _index6 = _interopRequireDefault(_index5);
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var _components = {
+    Header: {
+        displayName: 'Header'
+    }
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+var _UsersEdwardsztukowskiNode_modulesReactTransformHmrLibIndexJs2 = (0, _index6.default)({
+    filename: 'src/app.js',
+    components: _components,
+    locals: [module],
+    imports: [_react3.default]
+});
+
+var _UsersEdwardsztukowskiNode_modulesReactTransformCatchErrorsLibIndexJs2 = (0, _index4.default)({
+    filename: 'src/app.js',
+    components: _components,
+    locals: [],
+    imports: [_react3.default, _index2.default]
+});
+
+function _wrapComponent(id) {
+    return function (Component) {
+        return _UsersEdwardsztukowskiNode_modulesReactTransformHmrLibIndexJs2(_UsersEdwardsztukowskiNode_modulesReactTransformCatchErrorsLibIndexJs2(Component, id), id);
+    };
+}
+
+var Header = _wrapComponent('Header')(function (_React$Component) {
+    _inherits(Header, _React$Component);
+
+    function Header() {
+        _classCallCheck(this, Header);
+
+        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+    }
+
+    _createClass(Header, [{
+        key: 'render',
+        value: function render() {
+            return React.createElement(
+                'p',
+                null,
+                'This is a header'
+            );
+        }
+    }]);
+
+    return Header;
+}(React.Component));
+
+var jsx = React.createElement(
+    'div',
     null,
-    app.title
-  ),
-  app.subtitle && React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.options.length > 0 ? 'Here are your options' : 'No Options'
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item 1'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item 2'
-    )
-  )
+    React.createElement(Header, null)
 );
 
-var count = 0;
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Count: ',
-    count
-  ),
-  React.createElement(
-    'button',
-    { className: 'btn' },
-    '+1'
-  )
-);
-
-var appRoot = document.getElementById("app");
-
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,30 +1,13 @@
-const app = {
-  title: 'Indecision App',
-  subtitle: 'Let computers think for you',
-  options: ['One', 'Two']
+class Header extends React.Component {
+    render() {
+        return <p>This is a header</p>;
+    }
 }
 
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
-    <ol>
-      <li>Item 1</li>
-      <li>Item 2</li>
-    </ol>
-  </div>
+const jsx = (
+    <div>
+        <Header />
+    </div>
 );
 
-let count = 0;
-
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button className='btn'>+1</button>
-  </div>
-)
-
-const appRoot = document.getElementById("app");
-
-ReactDOM.render(templateTwo, appRoot );
+ReactDOM.render(jsx, document.getElementById('app'));
